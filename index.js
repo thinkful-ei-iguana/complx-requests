550b071ba1e7255efad35233397be0a0c3e6c070
john94 <ethanok0802@gmail.com>
2019-11-01 11:54:28 -0700

    finished the assignment

--- index.js
+++ index.js
@@ -2,32 +2,51 @@ const API_KEY = "&api_key=wS90AN3ZMmmJsmsE4YiQoinn5g0ILLJ7IRBmYotI";
 const ENTRY_POINT = "https://developer.nps.gov/api/v1";
 
 const handleSearchResults = result => {
-  console.log(result);
+    let renderParks = '<h1>Here is the results:</h1>';
+    result.data.forEach(ele => {
+        let fullName = ele.fullName;
+        let description = ele.description;
+        let webUrl = ele.url;
+
+        $('.result').html(
+            renderParks +=
+            `<div class = 'container'>
+          <h2>Name: ${fullName}</h2>
+          <p>Description: ${description}</p>
+          <a href='${webUrl}'>Link</a>
+        </div>`
+        );
+
+
+    });
+    console.log(result);
 };
 
 const searchApi = params => {
-  fetch(
-    `${ENTRY_POINT}/parks?stateCode=${params.stateCode}&limit=${
+    fetch(
+            `${ENTRY_POINT}/parks?stateCode=${params.stateCode}&limit=${
       params.resultCount
     }${API_KEY}`
-  )
-    .then(result => result.json())
-    .then(resp => handleSearchResults(resp));
+        )
+        .then(result => result.json())
+        .then(resp => handleSearchResults(resp));
 };
 
 searchForm.addEventListener("submit", event => {
-  event.preventDefault();
-  const search = {
-    stateCode: stateCode.value,
-    resultCount: resultCount.value
-  };
+    event.preventDefault();
+    const search = {
+        stateCode: stateCode.value,
+        resultCount: resultCount.value
+    };
 
-  if (search.stateCode.length == 0 || resultCount < 1)
-    throw new Error("invalid search parameters");
 
-  searchApi(search);
 
-  console.log("search params", search);
+    if (search.stateCode.length === 0 || resultCount < 1)
+        throw new Error("invalid search parameters");
+
+    searchApi(search);
+
+    console.log("search params", search);
 });
 
 console.log("hello");
